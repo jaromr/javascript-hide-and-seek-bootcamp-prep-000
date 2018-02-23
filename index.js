32cf27984455585b4a92d50810401d03d3bde44b
Jarom Roney <byutrueblue@gmail.com>
2018-02-23 20:00:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ function getFirstSelector(selector){
   return document.querySelector(selector)
 }
 function nestedTarget(target, nested){
- var elements = document.querySelectorAll('#')
+ var elements = document.querySelectorAll('#nested')
+ console.log(elements)
 }
+
+ var elements = document.querySelectorAll('#nested')
+ console.log(elements)
