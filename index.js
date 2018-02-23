ee4f599cac6d46045db6aff501dc432bd6083dbf
Jarom Roney <byutrueblue@gmail.com>
2018-02-23 21:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function nestedTarget(target, nested){
 function increaseRankBy(n){
   const elements = document.querySelectorAll('ul.ranked-list')
   for (let i =0; i < elements.length; i++){
-    elements[i]=parseInt(elements[i],10) + n
+    elements[i].innerHTML=parseInt(elements[i],10) + n
   }
 }
