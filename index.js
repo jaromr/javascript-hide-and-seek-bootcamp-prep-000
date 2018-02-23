1a91f1cee13ef36198e4bf64fb8a2dfcaedd3a08
Jarom Roney <byutrueblue@gmail.com>
2018-02-23 22:31:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(target, nested){
+ return document.querySelector('#nested .target')
+}
+function increaseRankBy(n){
+  const elements = document.querySelectorAll('ul.ranked-list')
+  for (let i =0; i < elements.length; i++){
+    elements[i].innerHTML=parseInt(elements[i],10) + n
+  }
+}
+function deepestChild(){
+  var current = document.querySelector('div#grand-node')
+  while (typeof current.children[0] === 'object'){
+ 
+   current = current.children[0] 
+  }
+  return current
+}
